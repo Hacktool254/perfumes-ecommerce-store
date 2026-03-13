5fdef03ba3061bb619725e1e5aab0a1069846d96
Hacktool254 <97397752+Hacktool254@users.noreply.github.com>
2026-03-13 23:58:56 +0300

    Phase 16.1: Extract shared UI components into packages/ui

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,12 @@
+export * from "./badge";
+export * from "./button";
+export * from "./card";
+export * from "./dropdown-menu";
+export * from "./form";
+export * from "./input";
+export * from "./label";
+export * from "./select";
+export * from "./table";
+export * from "./textarea";
+
+export * from "./lib/utils";
